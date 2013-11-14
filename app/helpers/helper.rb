cee72b6fbfe85a3cfce66cad3bd855ef6f649669
Louie Perez <louie.perez3@gmail.com>
2013-11-14 15:42:05 -0800

    Spike

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,11 +1,11 @@
 module Twilioer
   
   def start_client
-    env_config = YAML.load_file(APP_ROOT.join('config', 'twilio.yaml'))
+    # env_config = YAML.load_file(APP_ROOT.join('config', 'twilio.yaml'))
 
-    env_config.each do |key, value|
-      ENV[key] = value
-    end
+    # env_config.each do |key, value|
+    #   ENV[key] = value
+    # end
 
     Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
   end
