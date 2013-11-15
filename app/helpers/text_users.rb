5eb43ea6560f64b165d22ef38590de9452056407
Louie Perez <louie.perez3@gmail.com>
2013-11-14 21:31:11 -0800

    Added texting module

--- app/helpers/text_users.rb
+++ app/helpers/text_users.rb
@@ -1,13 +1,23 @@
 def texting
-  p sunset_time = Sunset.last.sunset_time
 
-  p message = AddonText.happy + ' Tonight\'s sunset is at ' + sunset_time
+  sunset_time = Sunset.last.sunset_time
 
-  p all_users = User.all
-  # client = Twilioer.start_client
-  # client.account.messages.create(
-  #   :from => "17146602442",
-  #   :to => User.find_by_id(session[:id]).phone_number,
-  #   :body => message
-  #   )
+  message = AddonText.happy + ' Tonight\'s sunset is at ' + sunset_time
+
+  user_phone_numbers = []
+
+  User.all.each do |user|
+    user_phone_numbers << user.phone_number
+  end
+
+  user_phone_numbers.each do |phone_number|
+  client = Twilioer.start_client
+  client.account.messages.create(
+    :from => "17146602442",
+    :to => phone_number,
+    :body => message
+    )
+  end
+
 end
+
