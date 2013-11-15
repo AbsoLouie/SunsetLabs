b571ed9ae2ece7b71034a8633b4b0d947f8a4e8e
Louie Perez <louie.perez3@gmail.com>
2013-11-14 17:36:32 -0800

    Reincluded Twilio

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -57,16 +57,14 @@ post "/users/text_messages/sunset" do
 
   sunset_time = Sunset.last.sunset_time
 
-  # p HappyThoughts.happy
+  message = AddonText.happy + ' Tonight\'s sunset is at ' + sunset_time
 
-  p AddonText.happy + ' Tonight\'s sunset is at ' + sunset_time
-
-  # client = Twilioer.start_client
-  # client.account.messages.create(
-  #   :from => "17146602442",
-  #   :to => User.find_by_id(session[:id]).phone_number,
-  #   :body => HappyThoughts.happy +  sunset_time
-  #   )
+  client = Twilioer.start_client
+  client.account.messages.create(
+    :from => "17146602442",
+    :to => User.find_by_id(session[:id]).phone_number,
+    :body => message
+    )
 
   redirect '/'
 end
